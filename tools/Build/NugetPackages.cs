ef8dd674dac11651d1f699d151f471317830c88c
Ed Ball <ed.ball@logos.com>
2026-05-25 17:04:06 -0700

    Delete unused file

--- tools/Build/NugetPackages.cs
+++
@@ -1,9 +0,0 @@
-internal static class NugetPackages
-{
-	public static readonly IReadOnlyList<string> ProjectsToPublish = new[]
-	{
-		@"src\Faithlife.Testing",
-		@"src\Faithlife.Testing.RabbitMq",
-		@"src\Faithlife.Testing.WebRequests",
-	};
-}
